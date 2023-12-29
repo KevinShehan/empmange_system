4e212e5188bf4c7bc679259cdf0edc7ce26075a8
Kevin <kevin.shehan30@gmail.com>
2023-12-29 15:31:49 +0530

    button comp finished

--- my-project/src/components/EmployeeList.jsx
+++ my-project/src/components/EmployeeList.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 const EmployeeList = () => {
   return (
-    <>
+    <div className='container mx-auto my-6'>
       <div className='h-12'>
-        <button className='rounded bg-slate-600 text-white px-6 py-2'>Add Employee</button>
+        <button className='font-semibold rounded bg-slate-600 text-white px-6 py-2'>Add Employee</button>
+      </div>
+      <div className='flex shadow border-b'>
         <table>
+          <thead>
+            
+          </thead>
+          <tbody>
 
+          </tbody>
         </table>
       </div>
-    </>
+    </div>
 
   )
 }
